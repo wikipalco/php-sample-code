0af768955bafa728aa09c6ec14d44e915b088dfb
ltiny <lizard.tiny@gmail.com>
2016-12-01 19:40:12 +0330

    https://wikipal.co PHP Sample Code

---
+++ request.php
@@ -0,0 +1,23 @@
+﻿<?php
+$MerchantID 			= "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
+$Price 					= 100;
+$Description 			= "تراکنش شماره 15";
+$InvoiceNumber 			= 15;
+$CallbackURL 			= "http://example.com/verify.php";
+	
+$curl = curl_init();
+curl_setopt($curl, CURLOPT_URL, 'http://gatepay.co/webservice/paymentRequest.php');
+curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type' => 'application/json'));
+curl_setopt($curl, CURLOPT_POSTFIELDS, "MerchantID=$MerchantID&Price=$Price&Description=$Description&InvoiceNumber=$InvoiceNumber&CallbackURL=". urlencode($CallbackURL));
+curl_setopt($curl, CURLOPT_TIMEOUT, 400);
+curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+$result = json_decode(curl_exec($curl));
+curl_close($curl);
+
+if ($result->Status == 100){
+	header('Location: http://gatepay.co/webservice/startPayment.php?au='. $result->Authority);
+} else {
+	echo $result->Status;
+}
+?>
---
+++ verify.php
@@ -0,0 +1,28 @@
+﻿<?php
+$MerchantID 			= "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
+$Price 					= 100;
+$Authority 				= $_POST['authority'];
+$InvoiceNumber 			= $_POST['InvoiceNumber'];
+
+if ($_POST['status'] == 1) {
+
+	$curl = curl_init();
+	curl_setopt($curl, CURLOPT_URL, 'http://gatepay.co/webservice/paymentVerify.php');
+	curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type' => 'application/json'));
+	curl_setopt($curl, CURLOPT_POSTFIELDS, "MerchantID=$MerchantID&Price=$Price&Authority=$Authority");
+	curl_setopt($curl, CURLOPT_TIMEOUT, 400);
+	curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+	curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+	$result = json_decode(curl_exec($curl));
+	curl_close($curl);
+
+	if ($result->Status == 100) {
+		echo "Transaction Invoice Number $InvoiceNumber Success. Transaction Tracking Code : ".$result->RefCode;
+	} else {
+		echo $result->Status;
+	}
+
+} else {
+	echo 'Transaction Canceled By User';
+}
+?>
